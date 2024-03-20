845c90598dbe1af4c6cbe17b81468e07f5d0440b
nguyenston <phucnguyen1999.nn@gmail.com>
2024-03-20 17:29:11 -0400

    changed main

--- src/main.jl
+++ src/main.jl
@@ -12,27 +12,57 @@ using .StareNMF.Utils
 using CairoMakie
 CairoMakie.activate!(type="svg")
 
-function rho_k_losses(gridpos, losses, rhos; krange=1:length(losses), plot_title="")
-  subfig = GridLayout()
-  ax = Axis(gridpos, yscale=log10, title=plot_title)
-  for k in krange
-    lines!(ax, rhos, losses[k], label="k=$(k)")
-  end
-
-  subfig[1, 1] = ax
-  subfig[1, 2] = Legend(gridpos, ax, "Legend")
-  gridpos[] = subfig
-end
-
-function rank_determination(X, ks, rhos; approx_type=StareNMF.KDEUniform, nmfargs=(), plotargs=(), kwargs...)
-  losses = Array{Vector{Float64}}(undef, length(ks))
+function rank_determination(X, ks; nmfargs=())
   results = Array{NMF.Result}(undef, length(ks))
   for (i, k) in collect(enumerate(ks))
     result = threaded_nmf(Float64.(X), k; alg=:multdiv, maxiter=200000, tol=1e-4, nmfargs...)
-    losses[i] = StareNMF.structurally_aware_loss(X, result.W, result.H, rhos; lambda=0.01, approx_type, kwargs...)
     results[i] = result
   end
-  rho_k_losses(losses, rhos; plotargs..., krange=ks), losses, results
+  results
+end
+
+function cache_result(overwrite=false)
+  println("program start...")
+  signatures_unsorted = CSV.read("../synthetic-data-2023/alexandrov2015_signatures.tsv", DataFrame; delim='\t')
+  signatures = sort(signatures_unsorted)
+  cancer_categories = Dict(
+    "skin" => "107-skin-melanoma-all-seed-1",
+    "ovary" => "113-ovary-adenoca-all-seed-1",
+    "breast" => "214-breast-all-seed-1",
+    "liver" => "326-liver-hcc-all-seed-1",
+    "lung" => "38-lung-adenoca-all-seed-1",
+    "stomach" => "75-stomach-adenoca-all-seed-1")
+  misspecification_type = Dict(
+    "none" => "",
+    "contaminated" => "-contamination-2",
+    "overdispersed" => "-overdispersed-2.0",
+    "perturbed" => "-perturbed-0.0025")
+  nmf_algs = ["multdiv", "greedycd"]
+
+  println("start looping...")
+  for nmf_alg in nmf_algs, cancer in keys(cancer_categories)
+    loadings = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])-GT-loadings.csv", DataFrame; header=0)
+    nloadings = nrow(loadings)
+
+    for misspec in keys(misspecification_type)
+      println("alg: $(nmf_alg)\tcancer: $(cancer)\tmisspec: $(misspec)")
+      if isfile("../result-cache/cache-$(nmf_alg)-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2") && !overwrite
+        continue
+      end
+
+      data = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])$(misspecification_type[misspec]).tsv", DataFrame; delim='\t')
+      X = Matrix(data[:, 2:end])
+
+      results = rank_determination(X, 1:nloadings+3;
+        nmfargs=(; alg=:multdiv, replicates=16, ncpu=16))
+      componentwise_losses = Vector{Vector{Float64}}(undef, length(results))
+      Threads.@threads for i in eachindex(results)
+        r = results[i]
+        componentwise_losses[i] = componentwise_loss(X, r.W, r.H; approxargs=(; multiplier=500))
+      end
+      jldsave("../result-cache/cache-$(nmf_alg)-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2"; results, componentwise_losses)
+    end
+  end
 end
 
 function main()
@@ -51,36 +81,58 @@ function main()
     "contaminated" => "-contamination-2",
     "overdispersed" => "-overdispersed-2.0",
     "perturbed" => "-perturbed-0.0025")
+  nmf_algs = ["multdiv", "greedycd"]
 
   println("start looping...")
-  for cancer in keys(cancer_categories)
+  for nmf_alg in nmf_algs, cancer in keys(cancer_categories)
     loadings = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])-GT-loadings.csv", DataFrame; header=0)
     nloadings = nrow(loadings)
 
     for misspec in keys(misspecification_type)
-      println("cancer: $(cancer)\tmisspec: $(misspec)")
-      if isfile("../result-cache/cache-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2")
-        continue
+      println("alg: $(nmf_alg)\tcancer: $(cancer)\tmisspec: $(misspec)")
+      # jldsave("../result-cache/rho-k-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2"; rhos, ks, losses, results)
+      # data = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])$(misspecification_type[misspec]).tsv", DataFrame; delim='\t')
+      # X = Matrix(data[:, 2:end])
+
+      file = load("../result-cache/cache-$(nmf_alg)-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2")
+      results = [r for r in file["results"]]
+      componentwise_losses = file["componentwise_losses"]
+
+      valid_results = filter(results) do r
+        size(r.H)[1] <= nloadings
       end
+      fig = Figure(size=(3200, 1600))
+      rhos = 0:0.01:40
 
-      data = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])$(misspecification_type[misspec]).tsv", DataFrame; delim='\t')
-      X = Matrix(data[:, 2:end])
-
-      file = load("../result-cache/rho-k-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2")
-      results = file["results"]
-      ks = file["ks"]
-      rhos = file["rhos"]
-      losses = file["losses"]
-
-      componentwise_losses = Vector{Vector{Float64}}(undef, length(ks))
-      Threads.@threads for i in eachindex(results)
-        r = results[i]
-        componentwise_losses[i] = componentwise_loss(X, r.W, r.H; multiplier=500)
-      end
-      jldsave("../result-cache/cache-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2"; results, componentwise_losses)
+      rho_k_losses(fig[1, 2][1, 1], componentwise_losses, rhos)
+      rho_k_bottom(fig[1, 2][2, 1], componentwise_losses)
+      subfig_bubs, ax_bubs = bubbles(fig[1, 1], loadings, signatures, valid_results)
+      bubs_legend_and_colorbar = GridLayout()
+      bubs_legend_and_colorbar[1:2, 1] = subfig_bubs.content[2].content.content .|> x -> x.content
 
 
-      # save("../plots/composite/composite-$(cancer_categories[cancer])$(misspecification_type[misspec]).svg", fig)
+      ax1 = Axis(fig; yscale=log10, xticks=(1.5:length(valid_results)+0.5, ["K = $(i)" for i in 1:length(valid_results)]))
+      ax2 = Axis(fig; yscale=identity, xticks=(1.5:length(valid_results)+0.5, ["K = $(i)" for i in 1:length(valid_results)]))
+
+      mrl_maxes = score_by_cosine_difference.([loadings], [signatures], valid_results)
+      lines!(ax1, 1.5:length(valid_results)+0.5, [mm[1] for mm in mrl_maxes]; label="max relative loading difference")
+      lines!(ax2, 1.5:length(valid_results)+0.5, [mm[2] for mm in mrl_maxes]; label="max difference", color=:orange)
+      linkxaxes!(ax_bubs, ax1, ax2)
+      subfig_bubs[1, 1] = ax1
+      subfig_bubs[2, 1] = ax2
+      subfig_bubs[3, 1] = ax_bubs
+
+
+      subfig_bubs[1, 2] = Legend(fig, ax1)
+      subfig_bubs[2, 2] = Legend(fig, ax2)
+      subfig_bubs[3, 2] = bubs_legend_and_colorbar
+
+      rowsize!(subfig_bubs, 3, Relative(1 // 2))
+
+      fig[0, :] = Label(fig, "$(cancer_categories[cancer])$(misspecification_type[misspec])", fontsize=30)
+
+      save("../plots/composite-$(nmf_alg)/composite-$(nmf_alg)-$(cancer_categories[cancer])$(misspecification_type[misspec]).pdf", fig)
+      save("../plots/composite-$(nmf_alg)/composite-$(nmf_alg)-$(cancer_categories[cancer])$(misspecification_type[misspec]).svg", fig)
     end
   end
 end
