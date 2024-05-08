e059ccbf06c0429e75ae0a29e809f62c70f5f97b
nguyenston <phucnguyen1999.nn@gmail.com>
2024-05-08 04:20:18 -0400

    bayes nmf scripts

---
+++ src/cache-bayesNMF.jl
@@ -0,0 +1,52 @@
+using StanSample
+using Distributions
+using LinearAlgebra
+using NMF
+using DataFrames
+using CSV
+using JLD2
+
+function main(; overwrite=false)
+  println("program start...")
+  cancer_categories = Dict(
+    "skin" => "107-skin-melanoma-all-seed-1",
+    "ovary" => "113-ovary-adenoca-all-seed-1",
+    "breast" => "214-breast-all-seed-1",
+    "liver" => "326-liver-hcc-all-seed-1",
+    "lung" => "38-lung-adenoca-all-seed-1",
+    "stomach" => "75-stomach-adenoca-all-seed-1")
+  misspecification_type = Dict(
+    "none" => "",
+    "contaminated" => "-contamination-2",
+    "overdispersed" => "-overdispersed-2.0",
+    "perturbed" => "-perturbed-0.0025")
+  stan_program = read("poisson-nmf.stan", String)
+  hyperpriors = CSV.read("../synthetic-data-2023/bayes-nmf-hyperprior.tsv", DataFrame; delim="\t")
+  Base.Filesystem.mkpath("../raw-cache-stan/synthetic/")
+  println("start looping...")
+  for cancer in keys(cancer_categories)
+    loadings = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])-GT-loadings.csv", DataFrame; header=0)
+    nloadings = nrow(loadings)
+
+    for misspec in keys(misspecification_type)
+      if isfile("../raw-cache-stan/synthetic/cache-stan-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2") && !overwrite
+        continue
+      end
+
+      data = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])$(misspecification_type[misspec]).tsv", DataFrame; delim='\t')
+      X = Matrix(data[:, 2:end])
+      results = Dict()
+      for K in 1:nloadings+3
+        println("cancer: $(cancer)\tmisspec: $(misspec)\tK: $(K)")
+        hp = hyperpriors[:, "$(cancer)-$(misspec)"]
+        stan_data = (; I=size(X, 1), J=size(X, 2), K, alpha=1.0, gamma0=2.0, gamma1=4.0, delta0=hp[1], delta1=hp[2])
+        model = SampleModel("model-$(cancer)-$(misspec)", stan_program)
+
+        _ = stan_sample(model; stan_data, num_chains=16, num_samples=1000, num_warmups=4000, delta=0.98, max_depth=12)
+        results[K] = read_samples(model, :dataframe)
+      end
+
+      jldsave("../raw-cache-stan/synthetic/cache-stan-$(cancer_categories[cancer])$(misspecification_type[misspec]).jld2"; results)
+    end
+  end
+end
---
+++ src/poisson-nmf.stan
@@ -0,0 +1,46 @@
+data {
+  int<lower=1> J;
+  int<lower=1> I;    
+  int<lower=1> K;
+  int<lower=0> X[I, J];
+  real<lower=0> alpha;
+  real<lower=0> gamma0;  // shape parameters
+  real<lower=0> gamma1;  // scale parameters
+  real<lower=0> delta0;  // shape parameters for mean loadings
+  real<lower=0> delta1;  // scale parameters for mean loadings
+}
+        
+transformed data {
+  vector<lower=0>[I] alpha_array = rep_vector(alpha, I);
+}
+
+parameters {
+  vector<lower=0>[K] nu;
+  vector<lower=0>[K] mu;
+  matrix<lower=0>[K, J] theta;
+  simplex[I] r[K];
+}
+        
+model {
+  real mutation_rate;
+  for (k in 1:K) {
+    nu[k] ~ inv_gamma(gamma0, gamma1);
+    mu[k] ~ inv_gamma(delta0, delta1);
+    r[k] ~ dirichlet(alpha_array);
+  }
+  for (j in 1:J) {
+    for (k in 1:K) {
+      theta[k, j] ~ gamma(nu[k], nu[k]/mu[k]);
+    }
+  }
+
+  for (j in 1:J){
+    for (i in 1:I) {
+      mutation_rate = 0;
+      for (k in 1:K){
+          mutation_rate += r[k, i] * theta[k, j];
+      }
+      target += -mutation_rate + X[i, j]*log(mutation_rate);
+    }
+  }
+}
