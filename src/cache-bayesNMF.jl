b928a05d8a93572ce4e2d6da8a75d7435881a336
nguyenston <phucnguyen1999.nn@gmail.com>
2024-05-12 00:27:14 -0400

    update script parameters

--- src/cache-bayesNMF.jl
+++ src/cache-bayesNMF.jl
@@ -40,9 +40,9 @@ function main(; overwrite=false)
         println("cancer: $(cancer)\tmisspec: $(misspec)\tK: $(K)")
         hp = hyperpriors[:, "$(cancer)-$(misspec)"]
         stan_data = (; I=size(X, 1), J=size(X, 2), K, X, alpha=1.0, gamma0=2.0, gamma1=4.0, delta0=hp[1], delta1=hp[2])
-        model = SampleModel("model-$(cancer)-$(misspec)", stan_program)
+        model = SampleModel("model-$(cancer)-$(misspec)-$(K)", stan_program)
 
-        _ = stan_sample(model; data=stan_data, num_chains=16, num_samples=1000, num_warmups=4000, delta=0.98, max_depth=12)
+        _ = stan_sample(model; data=stan_data, num_chains=4, num_samples=1000, num_warmups=4000, delta=0.98, max_depth=12, show_logging=true)
         results[K] = read_samples(model, :dataframe)
       end
 
