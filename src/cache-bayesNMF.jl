255a0b46d952d26a6f68c0a53c76ec865566a73e
nguyenston <phucnguyen1999.nn@gmail.com>
2024-05-15 13:52:23 -0400

    K is now a cmd line argument

--- src/cache-bayesNMF.jl
+++ src/cache-bayesNMF.jl
@@ -6,15 +6,16 @@ using DataFrames
 using CSV
 using JLD2
 
-function main(; overwrite=false)
+function main(K; overwrite=false)
   println("program start...")
   cancer_categories = Dict(
-    "skin" => "107-skin-melanoma-all-seed-1",
-    "ovary" => "113-ovary-adenoca-all-seed-1",
+    # "skin" => "107-skin-melanoma-all-seed-1",
+    # "ovary" => "113-ovary-adenoca-all-seed-1",
     "breast" => "214-breast-all-seed-1",
-    "liver" => "326-liver-hcc-all-seed-1",
+    # "liver" => "326-liver-hcc-all-seed-1",
     "lung" => "38-lung-adenoca-all-seed-1",
-    "stomach" => "75-stomach-adenoca-all-seed-1")
+    # "stomach" => "75-stomach-adenoca-all-seed-1"
+  )
   misspecification_type = Dict(
     "none" => "",
     "contaminated" => "-contamination-2",
@@ -35,19 +36,16 @@ function main(; overwrite=false)
 
       data = CSV.read("../synthetic-data-2023/synthetic-$(cancer_categories[cancer])$(misspecification_type[misspec]).tsv", DataFrame; delim='\t')
       X = Matrix{Int}(data[:, 2:end])
-      for K in 1:nloadings+3
-        println("cancer: $(cancer)\tmisspec: $(misspec)\tK: $(K)")
-        hp = hyperpriors[:, "$(cancer)-$(misspec)"]
-        stan_data = (; I=size(X, 1), J=size(X, 2), K, X, alpha=1.0, gamma0=2.0, gamma1=4.0, delta0=hp[1], delta1=hp[2])
-        model = SampleModel("model-$(cancer)-$(misspec)-$(K)", stan_program)
+      println("cancer: $(cancer)\tmisspec: $(misspec)\tK: $(K)")
+      hp = hyperpriors[:, "$(cancer)-$(misspec)"]
+      stan_data = (; I=size(X, 1), J=size(X, 2), K, X, alpha=1.0, gamma0=2.0, gamma1=4.0, delta0=hp[1], delta1=hp[2])
+      model = SampleModel("model-$(cancer)-$(misspec)-$(K)", stan_program)
 
-        _ = stan_sample(model; data=stan_data, num_chains=4, num_samples=1000, num_warmups=4000, delta=0.98, max_depth=12, show_logging=true)
-        result = read_samples(model, :dataframe)
-        jldsave("../raw-cache-stan/synthetic/cache-stan-$(cancer_categories[cancer])$(misspecification_type[misspec])-$(K).jld2"; result)
-      end
-
+      _ = stan_sample(model; data=stan_data, num_chains=4, num_samples=1000, num_warmups=4000, delta=0.98, max_depth=12, show_logging=true)
+      result = read_samples(model, :dataframe)
+      jldsave("../raw-cache-stan/synthetic/cache-stan-$(cancer_categories[cancer])$(misspecification_type[misspec])-$(K).jld2"; result)
     end
   end
 end
 
-main()
+main(ARGS[1])
