19511b8c0390d89f9d8f85148099340d1c7088fa
nguyenston <phucnguyen1999.nn@gmail.com>
2024-06-30 14:12:03 -0400

    fix function name for previous commit

--- src/StareNMF/utils/nmfutils.jl
+++ src/StareNMF/utils/nmfutils.jl
@@ -73,7 +73,7 @@ end
 custom_solve!(alg::NMF.GreedyCD{T}, X, W, H, simplex_H=false) where {T} =
   nmf_skeleton!(NMF.GreedyCDUpd{T}(alg.update_H, alg.lambda_w, alg.lambda_h), X, W, H, alg.maxiter, alg.verbose, alg.tol, simplex_H)
 
-function solve!(alg::NMF.MultUpdate{T}, X, W, H, simplex_H=false) where {T}
+function custom_solve!(alg::NMF.MultUpdate{T}, X, W, H, simplex_H=false) where {T}
 
   if alg.obj == :mse
     nmf_skeleton!(NMF.MultUpdMSE(alg.update_H, alg.lambda_w, alg.lambda_h, sqrt(eps(T))), X, W, H, alg.maxiter, alg.verbose, alg.tol, simplex_H)
